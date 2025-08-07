b79f4eb55ed88f1a531e5e76e0e869f6048b9370
Mirsulton #1 <91003008+Mirsulton1@users.noreply.github.com>
2025-08-06 22:50:52 -0400

    Update single_number.py

--- Single Number/single_number.py
+++ Single Number/single_number.py
@@ -1,16 +1,16 @@
 from typing import List
 
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        ans = [0] * (n + 1)
-        for i in range(1, n + 1):
-            ans[i] = ans[i >> 1] + (i & 1)
-        return ans
-    
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
 def main():
-    n = int(input("Enter n: "))
+    nums = list(map(int, input("Enter integers separated by space: ").split()))
     sol = Solution()
-    print(sol.countBits(n))
+    result = sol.singleNumber(nums)
+    print("Single number is:", result)
 
 if __name__ == "__main__":
     main()
