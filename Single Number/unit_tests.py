943dccf865d4564cc209d3c5386ed7e49c0fb8ec
Mirsulton #1 <91003008+Mirsulton1@users.noreply.github.com>
2025-08-06 22:51:07 -0400

    Update unit_tests.py

--- Single Number/unit_tests.py
+++ Single Number/unit_tests.py
@@ -1,21 +1,21 @@
 import unittest
-from single_number import Solution
 
-class UnitTests(unittest.TestCase):
+class TestSingleNumber(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    def test1(self):
-        self.assertEqual(self.sol.countBits(2), [0,1,1])
+    def test_example1(self):
+        self.assertEqual(self.sol.singleNumber([2,2,1]), 1)
 
-    def test2(self):
-        self.assertEqual(self.sol.countBits(5), [0,1,1,2,1,2])
+    def test_example2(self):
+        self.assertEqual(self.sol.singleNumber([4,1,2,1,2]), 4)
 
-    def test3(self):
-        self.assertEqual(self.sol.countBits(0), [0])
+    def test_example3(self):
+        self.assertEqual(self.sol.singleNumber([1]), 1)
 
-    def test4(self):
-        self.assertEqual(len(self.sol.countBits(100000)), 100001)
+    def test_large(self):
+        arr = list(range(1, 10000)) * 2 + [999999]
+        self.assertEqual(self.sol.singleNumber(arr), 999999)
 
 if __name__ == "__main__":
     unittest.main()
