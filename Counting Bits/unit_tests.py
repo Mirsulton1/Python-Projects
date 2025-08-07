06cecdda40457689ebace398f9b57aa560d26fcf
Mirsulton #1 <91003008+Mirsulton1@users.noreply.github.com>
2025-08-06 22:51:50 -0400

    Create unit_tests.py

---
+++ Counting Bits/unit_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from counting_bit import Solution
+
+class UnitTests(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test1(self):
+        self.assertEqual(self.sol.countBits(2), [0,1,1])
+
+    def test2(self):
+        self.assertEqual(self.sol.countBits(5), [0,1,1,2,1,2])
+
+    def test3(self):
+        self.assertEqual(self.sol.countBits(0), [0])
+
+    def test4(self):
+        self.assertEqual(len(self.sol.countBits(100000)), 100001)
+
+if __name__ == "__main__":
+    unittest.main()
